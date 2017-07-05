b5aaccf24bc3d599ef2717183724db7a1791069f
omarwaleed <omarpersonalprofile@gmail.com>
2017-07-06 00:47:19 +0200

    moved to express

--- app.js
+++ app.js
@@ -1,64 +1,46 @@
-const Hapi = require('hapi');
-const Path = require('path');
-const Inert = require('inert');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, 'public')
-            }
-        }
-    }
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-server.log(['error', 'database', 'read']);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-server.connection({
-	host: "localhost",
-	port: process.env.NODE_ENV || 3000
-})
-
-server.register(require('vision'), (err)=>{
-	if(err){
-		console.error(err);
-	}
-
-	server.views({
-		engines: {
-			ejs: require('ejs')
-		},
-		relativeTo: __dirname,
-		path: 'views'
-
-	})
-})
-
-server.route({
-	method: "GET",
-	path: "/",
-	handler: function(req, reply){
-		reply.view('home');
-	}
-})
-
-server.register(Inert, () => {});
-
-server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
-    }
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-server.start((err)=>{
-	if(err){
-		throw err;
-	}
-	console.log(`Serving on ${server.info.uri}`);
-})
+module.exports = app;
