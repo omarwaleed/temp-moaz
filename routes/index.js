522cdfbc8b5b2ba7bc199a54cecf68ad5711e6c2
omarwaleed <omarpersonalprofile@gmail.com>
2017-07-20 12:17:59 +0200

    multiple changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Pens Gate' });
+router.get('/', function (req, res, next) {
+	res.render('home', {
+		title: 'Pens Gate'
+	});
+});
+
+router.get('/service/:name', function (req, res) {
+	res.render(('service_'+req.params.name), {
+		title: req.params.name
+	})
 });
 
 module.exports = router;
