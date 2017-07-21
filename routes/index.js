ec3b2f62ce31b36a5b4c744092df07088f9d3ed8
omarwaleed <omarpersonalprofile@gmail.com>
2017-07-21 21:30:50 +0200

    added pages for about contact and portfolio with their routes

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,18 @@ router.get('/', function (req, res, next) {
 	});
 });
 
+router.get('/portfolio', function(req, res){
+	res.render('portfolio');
+});
+
+router.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+router.get('/about', function(req, res){
+	res.render('about');
+});
+
 router.get('/service/:name', function (req, res) {
 	res.render(('service_'+req.params.name), {
 		title: req.params.name
