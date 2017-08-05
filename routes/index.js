6c03b462faf0cc47174a5a4955923c90cda46a42
omarwaleed <omarpersonalprofile@gmail.com>
2017-08-06 01:05:49 +0200

    photography service done wit other minor changes

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/about', function(req, res){
 	res.render('about');
 });
 
-router.get('/service/:name', function (req, res) {
+router.get('/services/:name', function (req, res) {
 	res.render(('service_'+req.params.name), {
 		title: req.params.name
 	})
